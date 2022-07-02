e83fbe818974a0415d93a71065bf4988526cffc0
sambi0197 <leandro.irigoyen97@gmail.com>
2022-07-02 12:46:33 -0700

    airbnb

---
+++ models/city.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the City class."""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Class representing a City."""
+    state_id = ""
+    name = ""
